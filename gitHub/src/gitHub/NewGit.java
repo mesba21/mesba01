ceb8a9898e75f7e480a61f940326347b4b883637
Mesba <Mesba@DESKTOP-BFRSG5T.fios-router.home>
2017-10-31 02:05:44 -0700

    test3

---
+++ gitHub/src/gitHub/NewGit.java
@@ -0,0 +1,22 @@
+package gitHub;
+
+public class NewGit {
+
+	public static void main(String[] args) {
+		
+		int [] x = {1,2,3,4,5,6,7,8,9};
+		
+		for(int i=0; i<x.length; i++){
+		
+		if(x[i]==6){
+			System.out.println("avilable "+ x[i] );
+		}
+		else{
+			System.out.println("not avilable " + x[i]);
+		}
+			
+		
+
+	}
+
+	}}
